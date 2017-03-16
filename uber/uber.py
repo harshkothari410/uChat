73db65ff3436b841ebd10157eda646c3bccdc0db
harshkothari410 <harshkothari410@gmail.com>
2017-03-16 19:52:09 -0400

    added backend and uber app

---
+++ uber/uber.py
@@ -0,0 +1,60 @@
+from uber_rides.auth import AuthorizationCodeGrant
+from uber_rides.client import UberRidesClient
+from uber_rides.session import Session
+
+YOUR_CLIENT_ID = 'dkZUuhWTWVfrL1pEO7iM6Fueka0IteNF'
+YOUR_CLIENT_SECRET = '7ZMw7r_gvf70djT86qebOSfTofB_zNnwyRwnoE_l'
+YOUR_SERVER_TOKEN = '7jj733zk0UYqhcN5UjXsZFF9WPJ7Fkin6zSik9ZB'
+YOUR_REDIRECT_URL = 'http://localhost'
+YOUR_PERMISSION_SCOPES = ['profile', 'history', 'places', 'request', 'request_receipt']
+auth_flow = AuthorizationCodeGrant(
+    YOUR_CLIENT_ID,
+    YOUR_PERMISSION_SCOPES,
+    YOUR_CLIENT_SECRET,
+    YOUR_REDIRECT_URL,
+)
+
+access_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOltdLCJzdWIiOiJkYmQyM2I2NS00OTE1LTRkMTctODZlYi02MTdiZDc3YjMzMGUiLCJpc3MiOiJ1YmVyLXVzMSIsImp0aSI6IjYxYzczNDhmLTM2ZWEtNDVjNS1iMGVjLTExOWViMGI5ZWI0MiIsImV4cCI6MTQ5MjI5NDY0NiwiaWF0IjoxNDg5NzAyNjQ2LCJ1YWN0IjoiRk9CcGUwU0t6bURta1R4UWZud3VxMXN4b29oZkFJIiwibmJmIjoxNDg5NzAyNTU2LCJhdWQiOiJka1pVdWhXVFdWZnJMMXBFTzdpTTZGdWVrYTBJdGVORiJ9.laztOcN9BMCVszskRN6-i-cMjjrdsYcBJGrnyveA4YtIl3XPg8WAcNGCMqInAgMcx_tRJy4LPuVpWKdDp_cOI5dlYR0o4u9211CWSLL7QEN28mfvdw-J8-wcleaIgxOLlBJT5N0Dd0PVIo9uK84TvKhYQSPMST35RENGXyt-AnDvJaTeK4-VjP0bEJ7Lnm6WB1Ct9lX9panC5fUSlXcxy6X580CDYnj_QvK3p5jNxbYwJb4zlFGAre7DH4oiTnh17OHCO88vHn-j7AEsgt8mFtdDzYzC-K-dy5fpLU95Ec4cmN0Dlx_24RiO5Ip6ZEM4LgN_BH7ORP4IaeV6-0CEnw'
+
+
+
+#Destination : 40.502744, -74.448545
+auth_url = auth_flow.get_authorization_url()
+
+st = auth_url.find('state=')
+
+ns = auth_url[st+len('state='):]
+state = ns[:ns.find('&')]
+print auth_url
+print state
+
+# redirect_uri = YOUR_REDIRECT_URL + '?code=' + access_token  + '&state=' + state
+
+# http://localhost/?state=o0tbQ7VtRzzBYtn0OALmjQY3XATB9UaU&code=fKW6qXnhmL1Naz6NvQ5TRwFkbZinNQ#_
+
+redirect_uri = 'http://localhost/?state='+ state + '&code=5kX9HspPBV9WfVp0fi7NT3yf7OURpj#_'
+session = auth_flow.get_session(redirect_uri)
+client = UberRidesClient(session)
+credentials = session.oauth2credential
+
+print credentials
+
+
+# session = Session(server_token=YOUR_SERVER_TOKEN)
+# client = UberRidesClient(session)
+response = client.get_products(40.488101, -74.437934)
+products = response.json.get('products')
+
+product_id = products[0].get('product_id')
+print products[0].get('display_name')
+estimate = client.estimate_ride(
+    product_id=product_id,
+    start_latitude=40.488101,
+    start_longitude=-74.437934,
+    end_latitude=40.502744,
+    end_longitude=-74.448545,
+    seat_count=2
+)
+
+fare = estimate.json.get('fare')
+print fare
