d9d1cceed260a20c8cdb4de763c3d659f1f87c76
Vidit Desai <vidit.desai08@gmail.com>
2017-04-15 22:24:48 -0400

    made changes to the code structure

--- uber/uber.py
+++ uber/uber.py
@@ -1,22 +1,26 @@
+"""
+	Uber Package for uChat
+"""
 
-import config
 from uber_rides.auth import AuthorizationCodeGrant
 from uber_rides.client import UberRidesClient
 from uber_rides.session import Session
+from uber import config
 
-# uber pool
-UFP_PRODUCT_ID = '26546650-e557-4a7b-86e7-6a3942445247'
-
-# uber black
-SURGE_PRODUCT_ID = 'd4abaae7-f4d6-4152-91cc-77523e8165a4'
-
-class Uber:
-	state=''
-	# auth_flow=''
+class Uber(object):
+	"""
+	Class for Uber package
+	"""
 
 	def __init__(self):
-		
-		self.auth_flow=AuthorizationCodeGrant(
+		"""
+		Initialization function
+		Generates Authorization URL when an object is created
+
+		"""	
+
+		# auth_flow :  OAuth 2.0 Authorization Code flow used to generate Authorization URL 
+		self.auth_flow = AuthorizationCodeGrant(
 
 			config.CLIENT_ID,
 			config.PERMISSION_SCOPES,
@@ -24,30 +28,70 @@ class Uber:
 			config.REDIRECT_URL
 
 			)
-		self.auth_url=self.auth_flow.get_authorization_url()
-		# FIX ME
+
+		# Authorization URL for user
+		self.auth_url = self.auth_flow.get_authorization_url()
+
+		# @fix me 
 		# access_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOltdLCJzdWIiOiJkYmQyM2I2NS00OTE1LTRkMTctODZlYi02MTdiZDc3YjMzMGUiLCJpc3MiOiJ1YmVyLXVzMSIsImp0aSI6IjYxYzczNDhmLTM2ZWEtNDVjNS1iMGVjLTExOWViMGI5ZWI0MiIsImV4cCI6MTQ5MjI5NDY0NiwiaWF0IjoxNDg5NzAyNjQ2LCJ1YWN0IjoiRk9CcGUwU0t6bURta1R4UWZud3VxMXN4b29oZkFJIiwibmJmIjoxNDg5NzAyNTU2LCJhdWQiOiJka1pVdWhXVFdWZnJMMXBFTzdpTTZGdWVrYTBJdGVORiJ9.laztOcN9BMCVszskRN6-i-cMjjrdsYcBJGrnyveA4YtIl3XPg8WAcNGCMqInAgMcx_tRJy4LPuVpWKdDp_cOI5dlYR0o4u9211CWSLL7QEN28mfvdw-J8-wcleaIgxOLlBJT5N0Dd0PVIo9uK84TvKhYQSPMST35RENGXyt-AnDvJaTeK4-VjP0bEJ7Lnm6WB1Ct9lX9panC5fUSlXcxy6X580CDYnj_QvK3p5jNxbYwJb4zlFGAre7DH4oiTnh17OHCO88vHn-j7AEsgt8mFtdDzYzC-K-dy5fpLU95Ec4cmN0Dlx_24RiO5Ip6ZEM4LgN_BH7ORP4IaeV6-0CEnw'
 
-		# Authorization URL
-		st = self.auth_url.find('state=')
-		ns = self.auth_url[st+len('state='):]
-		self.state = ns[:ns.find('&')]
+		# This section restores the state of the auth_url
+		state = self.auth_url.find('state=')
+		state_ns = self.auth_url[state+len('state='):]
+		self.state = state_ns[:state_ns.find('&')]
 
 	def get_login_url(self):
+		"""
+		Provides the authorization URL 
+		
+		@arguments
+			None 
+
+		@return
+			authorization url
+		"""
 		return self.auth_url
 
-	def get_products(self,redirect_uri,latitude,longitude):
+	def get_products(self, redirect_uri, latitude, longitude):
+		"""
+		returns available uber products with authorization 
+		
+		@arguments
+			redirect_uri : The the redirect url received after authorization 
+			latitude : Latitude of requested location
+			longitude : Longitude of requested location
+
+		@return
+			products : Return uber products for the given location
+		"""
+
 		# Creating Session
 		session = self.auth_flow.get_session(redirect_uri)
 
 		# creating client
 		client = UberRidesClient(session)
 
-		response = client.get_products(latitude,longitude)
+		response = client.get_products(latitude, longitude)
 		products = response.json.get('products')
 		return products
 
 	def get_estimate(self,redirect_uri,productId,source_longitude,source_latitude,dest_longitude,dest_latitude,count):
+		"""
+		Returns exact estimate for a ride 
+		
+		@arguments
+			redirect_uri : The the redirect url received after authorization 
+			productId : The product id for requested service
+			source_latitude : Latitude of source location
+			source_longitude : Longitude of source location
+			dest_latitude : Latitude of destination 
+			dest_longitude : Longitude of destination 
+			count : Number of passengers
+
+		@return
+			estimate : The exact estimate for the request
+		"""
+
 		session = self.auth_flow.get_session(redirect_uri)
 		client = UberRidesClient(session)
 		response = client.estimate_ride(
@@ -63,15 +107,37 @@ class Uber:
 		return estimate
 
 
-	def available_products(self,latitude,longitude):
+	def available_products(self, latitude, longitude):
+		"""
+		returns available uber products without authorization using server token 
+		
+		@arguments
+			latitude : Latitude of requested location
+			longitude : Longitude of requested location
+
+		@return
+			products : Return uber products for the given location
+		"""
 		session = Session(server_token=config.SERVER_TOKEN)
 		client = UberRidesClient(session)
-		response = client.get_products(latitude,longitude)
+		response = client.get_products(latitude, longitude)
 		products = response.json.get('products')
 		return products
 
-	def average_estimate(self,source_latitude,source_longitude,dest_latitude,dest_longitude,count):
+	def rough_estimate(self, source_latitude, source_longitude, dest_latitude, dest_longitude, count):
+		"""
+		Returns rough estimate for a ride 
+		
+		@arguments
+			source_latitude : Latitude of source location
+			source_longitude : Longitude of source location
+			dest_latitude : Latitude of destination 
+			dest_longitude : Longitude of destination 
+			count : Number of passengers
 
+		@return
+			estimate : The rough estimates for the request
+		"""
 		session = Session(server_token=config.SERVER_TOKEN)
 		client = UberRidesClient(session)
 		response = client.get_price_estimates(
@@ -85,19 +151,39 @@ class Uber:
 		estimate = response.json.get('prices')
 		return estimate
 			
-	def get_history(self,redirect_uri):
+	def get_history(self, redirect_uri):
+		"""
+		Returns previous user history
+
+		@arguments 
+			redirect_uri : The the redirect url received after authorization
+
+		@return
+			history : User Ride History
+		"""
 		session = self.auth_flow.get_session(redirect_uri)
 		client = UberRidesClient(session)
 		response = client.get_user_activity()
 		history = response.json
 		return history
 
-	def get_user_profile(self,redirect_uri):
+	def get_user_profile(self, redirect_uri):
+		"""
+		Returns user profile 
+
+		@arguments 
+			redirect_uri : The the redirect url received after authorization
+
+		@return
+			profile : The users profile
+
+		"""
 		session = self.auth_flow.get_session(redirect_uri)
 		client = UberRidesClient(session)
 		response = client.get_user_profile()
 		profile = response.json
 		return profile
+
 
 
 
